d581636579a7364a385dae512fc4fc05553da391
Rubén Alejandro <rubenalejandrosolerlopez@gmail.com>
2024-06-06 11:15:28 -0400

    menu hamburguesa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { SitiosWebCompartidosComponent } from './modules/maincontent/website/sit
 import { SupportComponent } from './modules/maincontent/support/support.component';
 import { EvaluacionSeoComponent } from './modules/maincontent/evaluacion-seo/evaluacion-seo.component'; // Importa tu nuevo componente
 import { AnalisisTelusComponent } from './modules/maincontent/analisis-telus/analisis-telus.component';
+import { TelusGrupalComponent } from './modules/maincontent/telus-grupal/telus-grupal.component';
 
 export const routes: Routes = [
     { path: 'sitios-web', component: WebsiteComponent, data: { title: 'Sitios Web', icon: 'pi pi-pen-to-square' } },
     { path: 'subscription', component: SubscriptionComponent, data: { title: 'Suscripción', icon: 'pi pi-envelope' } },
     { path: 'support', component: SupportComponent, data: { title: 'Acompañamiento', icon: 'pi pi-users' } },
+    { path: 'telus-grupal', component: TelusGrupalComponent, data: { title: 'Telus Grupal', icon: 'pi pi-chart-line' } }, // Agrega la nueva ruta
     { path: 'sitios-compartidos', component: SitiosWebCompartidosComponent },
     { path: 'evaluacion-seo', component: EvaluacionSeoComponent, data: { title: 'Evaluación SEO', icon: 'pi pi-hammer' } }, // Agrega la nueva ruta
     { path: 'analisis-telus', component: AnalisisTelusComponent, data: { title: 'Análisis Telus', icon: 'pi pi-chart-line' } }, // Agrega la nueva ruta
---
+++ src/app/modules/maincontent/telus-grupal/telus-grupal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-telus-grupal',
+  standalone: true,
+  imports: [],
+  templateUrl: './telus-grupal.component.html',
+  styleUrl: './telus-grupal.component.scss'
+})
+export class TelusGrupalComponent {
+
+}
