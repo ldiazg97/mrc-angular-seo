791f293095c17604400a009bc4bf36e73ef6df01
Rubén Alejandro <rubenalejandrosolerlopez@gmail.com>
2024-06-18 16:24:13 -0400

    el avioncito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { SupportComponent } from './modules/maincontent/support/support.componen
 import { EvaluacionSeoComponent } from './modules/maincontent/evaluacion-seo/evaluacion-seo.component'; // Importa tu nuevo componente
 import { AnalisisTelusComponent } from './modules/maincontent/analisis-telus/analisis-telus.component';
 import { TelusGrupalComponent } from './modules/maincontent/telus-grupal/telus-grupal.component';
+import { DiagnosticoSeoComponent } from './modules/maincontent/diagnostico-seo/diagnostico-seo.component';
 
 export const routes: Routes = [
     { path: 'sitios-web', component: WebsiteComponent, data: { title: 'Sitios Web', icon: 'pi pi-pen-to-square' } },
@@ -14,6 +15,7 @@ export const routes: Routes = [
     { path: 'subscription', component: SubscriptionComponent, data: { title: 'Suscripción', icon: 'pi pi-envelope' } },
     { path: 'telus-grupal', component: TelusGrupalComponent, data: { title: 'Telus Grupal', icon: 'pi pi-chart-line' } }, // Agrega la nueva ruta
     { path: 'support', component: SupportComponent, data: { title: 'Acompañamiento', icon: 'pi pi-users' } },
+    { path: 'diagnostico-seo', component: DiagnosticoSeoComponent, data: { title: 'Diagnóstico SEO', icon: 'pi pi-wrench' } },
     { path: 'sitios-web-compartidos', component: SitiosWebCompartidosComponent },
     { path: 'evaluacion-seo', component: EvaluacionSeoComponent, data: { title: 'Evaluación SEO', icon: 'pi pi-hammer' } }, // Agrega la nueva ruta
     { path: 'analisis-telus', component: AnalisisTelusComponent, data: { title: 'Análisis Telus', icon: 'pi pi-chart-line' } }, // Agrega la nueva ruta
---
+++ src/app/modules/maincontent/diagnostico-seo/diagnostico-seo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnostico-seo',
+  standalone: true,
+  imports: [],
+  templateUrl: './diagnostico-seo.component.html',
+  styleUrl: './diagnostico-seo.component.scss'
+})
+export class DiagnosticoSeoComponent {
+
+}
