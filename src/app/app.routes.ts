52c8e93bbc057697ce2becee433ebb1ddb3730c6
ldiazg97 <lesterd495@gmail.com>
2024-06-27 11:35:00 -0400

    ADD: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,87 @@ import { EvaluacionSeoComponent } from './modules/maincontent/evaluacion-seo/eva
 import { AnalisisTelusComponent } from './modules/maincontent/analisis-telus/analisis-telus.component';
 import { TelusGrupalComponent } from './modules/maincontent/telus-grupal/telus-grupal.component';
 import { DiagnosticoSeoComponent } from './modules/maincontent/diagnostico-seo/diagnostico-seo.component';
+import { SitesComponent } from './modules/navpanel/header/seguimiento/seguimiento-menu/sites/sites.component';
+import { VarValComponent } from './modules/navpanel/header/seguimiento/seguimiento-menu/var-val/var-val.component';
+import { CommentComponent } from './modules/navpanel/header/seguimiento/seguimiento-menu/comment/comment.component';
+import { WebmasterComponent } from './modules/navpanel/header/seguimiento/seguimiento-menu/webmaster/webmaster.component';
+import { SitesEnpruebaComponent } from './modules/navpanel/header/seguimiento/seguimiento-menu/sites-enprueba/sites-enprueba.component';
+import { SitesSendComponent } from './modules/navpanel/header/seguimiento/seguimiento-menu/sites-send/sites-send.component';
+import { AccesosComponent } from './modules/navpanel/header/seguimiento/auditoria/accesos/accesos.component';
+import { AyudaComponent } from './modules/navpanel/header/seguimiento/config-menu/ayuda/ayuda.component';
+import { BloquesComponent } from './modules/navpanel/header/seguimiento/config-menu/bloques/bloques.component';
+import { NotificacionesComponent } from './modules/navpanel/header/seguimiento/config-menu/notificaciones/notificaciones.component';
+import { PaquetesComponent } from './modules/navpanel/header/seguimiento/config-menu/paquetes/paquetes.component';
+import { SegTeamComponent } from './modules/navpanel/header/seguimiento/config-menu/seg-team/seg-team.component';
+import { SetenciasComponent } from './modules/navpanel/header/seguimiento/config-menu/setencias/setencias.component';
+import { SpecialPermissionComponent } from './modules/navpanel/header/seguimiento/config-menu/special-permission/special-permission.component';
+import { TerminosComponent } from './modules/navpanel/header/seguimiento/config-menu/terminos/terminos.component';
+import { TimeRangeComponent } from './modules/navpanel/header/seguimiento/config-menu/time-range/time-range.component';
 
-export const routes: Routes = [
-    { path: 'sitios-web', component: WebsiteComponent, data: { title: 'Sitios Web', icon: 'pi pi-pen-to-square' } },
-    { path: 'sitios-compartidos', component: SitiosCompartidosComponent, data: {title:'Sitios Compartidos', icon: 'pi pi-desktop'} },
-    { path: 'subscription', component: SubscriptionComponent, data: { title: 'Suscripción', icon: 'pi pi-envelope' } },
-    { path: 'telus-grupal', component: TelusGrupalComponent, data: { title: 'Telus Grupal', icon: 'pi pi-chart-line' } }, // Agrega la nueva ruta
-    { path: 'support', component: SupportComponent, data: { title: 'Acompañamiento', icon: 'pi pi-users' } },
-    { path: 'diagnostico-seo', component: DiagnosticoSeoComponent, data: { title: 'Diagnóstico SEO', icon: 'pi pi-wrench' } },
-    { path: 'sitios-web-compartidos', component: SitiosWebCompartidosComponent },
-    { path: 'evaluacion-seo', component: EvaluacionSeoComponent, data: { title: 'Evaluación SEO', icon: 'pi pi-hammer' } }, // Agrega la nueva ruta
-    { path: 'analisis-telus', component: AnalisisTelusComponent, data: { title: 'Análisis Telus', icon: 'pi pi-chart-line' } }, // Agrega la nueva ruta
-    { path: '', redirectTo: '/sitios-web', pathMatch: 'full' }
+export const misRoutes: Routes = [
+  {
+    path: 'sitios-web',
+    component: WebsiteComponent,
+    data: { title: 'Sitios Web', icon: 'pi pi-pen-to-square' },
+  },
+  {
+    path: 'sitios-compartidos',
+    component: SitiosCompartidosComponent,
+    data: { title: 'Sitios Compartidos', icon: 'pi pi-desktop' },
+  },
+  {
+    path: 'subscription',
+    component: SubscriptionComponent,
+    data: { title: 'Suscripción', icon: 'pi pi-envelope' },
+  },
+  {
+    path: 'telus-grupal',
+    component: TelusGrupalComponent,
+    data: { title: 'Telus Grupal', icon: 'pi pi-chart-line' },
+  }, // Agrega la nueva ruta
+  {
+    path: 'support',
+    component: SupportComponent,
+    data: { title: 'Acompañamiento', icon: 'pi pi-users' },
+  },
+  {
+    path: 'diagnostico-seo',
+    component: DiagnosticoSeoComponent,
+    data: { title: 'Diagnóstico SEO', icon: 'pi pi-wrench' },
+  },
+  { path: 'sitios-web-compartidos', component: SitiosWebCompartidosComponent },
+  {
+    path: 'evaluacion-seo',
+    component: EvaluacionSeoComponent,
+    data: { title: 'Evaluación SEO', icon: 'pi pi-hammer' },
+  }, // Agrega la nueva ruta
+  {
+    path: 'analisis-telus',
+    component: AnalisisTelusComponent,
+    data: { title: 'Análisis Telus', icon: 'pi pi-chart-line' },
+  }, // Agrega la nueva ruta
+  { path: '', redirectTo: '/sitios-web', pathMatch: 'full' },
 ];
+// menus seguimiento
+export const seguimiento: Routes = [
+    //seguimiento
+  { path: 'administrar/sitiosweb', component: SitesComponent },
+  { path: 'administrar/valoraciones', component: VarValComponent },
+  { path: 'administrar/comentario/listar', component: CommentComponent },
+  { path: 'administrar/webmaster/listar', component: WebmasterComponent },
+  { path: 'administrar/sitiosweb/prueba/gratis', component: SitesEnpruebaComponent},
+  { path: 'propiedad/listar/compartidas', component: SitesSendComponent },
+  //configuracion
+  {path:'administrar/sentencias', component:SetenciasComponent},
+  {path:'administrar/ayuda/listar', component:AyudaComponent},
+  {path:'administrar/terminos/listar', component:TerminosComponent},
+  {path:'administrar/comercial/listar', component:SegTeamComponent},
+  {path:'notificaciones/listar', component:NotificacionesComponent},
+  {path:'bloque/listar', component:BloquesComponent},
+  {path:'rango/listar', component:TimeRangeComponent},
+  {path:'paquete/listar', component:PaquetesComponent},
+  {path:'asignar_paquetes/listar', component:SpecialPermissionComponent},
+  //accseso
+  { path: 'administrar/accesos', component: AccesosComponent },
+];
+export const routes: Routes = [...misRoutes, ...seguimiento];
--- src/app/modules/navpanel/header/header.component.ts
+++ src/app/modules/navpanel/header/header.component.ts
@@ -6,91 +6,123 @@ import { CommonModule } from '@angular/common';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuModule } from 'primeng/menu';
-import { NavServiceService } from '../nav/nav-service.service';
+// import { NavServiceService } from '../nav/nav-service.service';
 import { NavService } from '../../../Services/nav.service';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    imports: [
-        SeguimientoComponent,
-        ButtonModule,
-        SplitButtonModule,
-        MenuModule, MenubarModule,
-        CommonModule
-    ]
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [
+    SeguimientoComponent,
+    ButtonModule,
+    SplitButtonModule,
+    MenuModule,
+    MenubarModule,
+    CommonModule,
+    RouterLink,
+    RouterModule,
+  ],
 })
-export class HeaderComponent implements OnInit{
-    menuVisible = false;
-    seguimientoItems: MenuItem[] = [];
-    configurarItems: MenuItem[] = [];
-    auditoriaItems: MenuItem[] =[];
+export class HeaderComponent implements OnInit {
+  menuVisible = false;
+  seguimientoItems: MenuItem[] = [];
+  configurarItems: MenuItem[] = [];
+  auditoriaItems: MenuItem[] = [];
 
-    //cerrar el sidebar
-    constructor(private navService: NavService){}
+  //cerrar el sidebar
+  constructor(private navService: NavService) {}
 
-    onButtonClick(){
-        console.log('El botón fue clickeado');
+  onButtonClick() {
+    console.log('El botón fue clickeado');
 
-        // this.navservice.toggleSidebar();
-    }
-  
-    ngOnInit() {
-      this.seguimientoItems = [
-        { label: 'Sitios Web', icon: 'pi pi-server' },
-        { label: 'Sitios Web Compartidos', icon: 'pi pi-share-alt' },
-        { label: 'Valoraciones de Variables', icon: 'pi pi-star-fill' },
-        { label: 'Comentarios', icon: 'pi pi-comments' },
-        { label: 'Webmasters', icon: 'pi pi-users' },
-        { label: 'Sitios Web de Prueba', icon: 'pi pi-file-word' }
-      ];
-  
-      this.configurarItems = [
-        { label: 'Sentencias', icon: 'pi pi-book' },
-        { label: 'Ayuda', icon: 'pi pi-question-circle' },
-        { label: 'Terminos', icon: 'pi pi-file-edit' },
-        { label: 'Equipo de Seguimiento', icon: 'pi pi-eye' },
-        { label: 'Notificaciones', icon: 'pi pi-bell' },
-        { label: 'Bloques', icon: 'pi pi-clone' },
-        { label: 'Rango de Tiempo', icon: 'pi pi-hourglass' },
-        { label: 'Paquetes', icon: 'pi pi-objects-column' },
-        { label: 'Permisos Especiales', icon: 'pi pi-lock' }
-      ];
-  
-      this.auditoriaItems = [
-        { label: 'Accesos', icon: 'pi pi-users' },
-      ];
-    }
-    toggleMenu() {
-        this.menuVisible = !this.menuVisible;
-    }
+    // this.navservice.toggleSidebar();
+  }
 
-    login() {
-        // Lógica de login aquí
-        console.log('Login clicked');
-    }
+  ngOnInit() {
+    this.seguimientoItems = [
+      {
+        label: 'Sitios Web',
+        icon: 'pi pi-server',
+        routerLink: '/administrar/sitiosweb',
+      },
+      {
+        label: 'Sitios Web Compartidos',
+        icon: 'pi pi-share-alt',
+        routerLink: '/propiedad/listar/compartidas',
+      },
+      {
+        label: 'Valoraciones de Variables',
+        icon: 'pi pi-star-fill',
+        routerLink: '/administrar/valoraciones',
+      },
+      {
+        label: 'Comentarios',
+        icon: 'pi pi-comments',
+        routerLink: '/administrar/comentario/listar',
+      },
+      {
+        label: 'Webmasters',
+        icon: 'pi pi-users',
+        routerLink: '/administrar/webmaster/listar',
+      },
+      {
+        label: 'Sitios Web de Prueba',
+        icon: 'pi pi-file-word',
+        routerLink: '/administrar/sitiosweb/prueba/gratis',
+      },
+    ];
 
-    logout() {
-        // Lógica de logout aquí
-        console.log('Logout clicked');
-    }
-    seguimientoClick() {
-        // Lógica para el clic en el botón de seguimiento
-        console.log('Seguimiento button clicked');
-    }
+    this.configurarItems = [
+      { label: 'Sentencias', icon: 'pi pi-book', routerLink:'/administrar/sentencias'},
+      { label: 'Ayuda', icon: 'pi pi-question-circle', routerLink:'/administrar/ayuda/listar' },
+      { label: 'Terminos', icon: 'pi pi-file-edit', routerLink:'/administrar/terminos/listar' },
+      { label: 'Equipo de Seguimiento', icon: 'pi pi-eye', routerLink:'/administrar/comercial/listar' },
+      { label: 'Notificaciones', icon: 'pi pi-bell', routerLink:'/notificaciones/listar' },
+      { label: 'Bloques', icon: 'pi pi-clone', routerLink:'/bloque/listar' },
+      { label: 'Rango de Tiempo', icon: 'pi pi-hourglass', routerLink:'/rango/listar' },
+      { label: 'Paquetes', icon: 'pi pi-objects-column', routerLink:'/paquete/listar' },
+      { label: 'Permisos Especiales', icon: 'pi pi-lock', routerLink:'/asignar_paquetes/listar' },
+    ];
 
-    configurarClick() {
-        // Lógica para el clic en el botón de configuración
-        console.log('Configurar button clicked');
-    }
+    this.auditoriaItems = [
+      {
+        label: 'Accesos',
+        icon: 'pi pi-users',
+        routerLink: '/administrar/accesos',
+      },
+    ];
+  }
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
 
-    auditoriaClick() {
-        // Lógica para el clic en el botón de auditoría
-        console.log('Auditoría button clicked');
-    }
-    toggleNav() {
-        this.navService.toggleNav();
-      }
+  login() {
+    // Lógica de login aquí
+    console.log('Login clicked');
+  }
+
+  logout() {
+    // Lógica de logout aquí
+    console.log('Logout clicked');
+  }
+  seguimientoClick() {
+    // Lógica para el clic en el botón de seguimiento
+    console.log('Seguimiento button clicked');
+  }
+
+  configurarClick() {
+    // Lógica para el clic en el botón de configuración
+    console.log('Configurar button clicked');
+  }
+
+  auditoriaClick() {
+    // Lógica para el clic en el botón de auditoría
+    console.log('Auditoría button clicked');
+  }
+  toggleNav() {
+    this.navService.toggleNav();
+  }
 }
